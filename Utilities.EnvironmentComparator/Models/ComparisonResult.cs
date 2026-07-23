eb6ae2c9b3c1010e56c22ecd7a5df793f56521a6
Jai <your-id+username@users.noreply.github.com>
2026-07-22 19:16:35 -0500

    fix(EnvironmentComparator): Update TotalCount calculation to recursively sum leaf nodes across Solution Explorer tree hierarchy

--- Utilities.EnvironmentComparator/Models/ComparisonResult.cs
+++ Utilities.EnvironmentComparator/Models/ComparisonResult.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Utilities.EnvironmentComparator.Models
 {
@@ -10,9 +11,27 @@ namespace Utilities.EnvironmentComparator.Models
         public List<DiffNode> AdminSettingsNodes { get; set; } = new();
         public List<DiffNode> MetadataNodes { get; set; } = new();
 
-        public int TotalCount => AdminSettingsNodes.Count + MetadataNodes.Count;
+        public int TotalCount => CountLeafNodes(AdminSettingsNodes) + CountLeafNodes(MetadataNodes);
         public int IdenticalCount { get; set; }
         public int DeltaCount { get; set; }
         public int UniqueCount { get; set; }
+
+        private static int CountLeafNodes(IEnumerable<DiffNode> nodes)
+        {
+            if (nodes == null) return 0;
+            int count = 0;
+            foreach (var node in nodes)
+            {
+                if (node.Children == null || node.Children.Count == 0)
+                {
+                    count++;
+                }
+                else
+                {
+                    count += CountLeafNodes(node.Children);
+                }
+            }
+            return count;
+        }
     }
 }
